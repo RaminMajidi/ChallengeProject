1571c9b6ecb5232919c44d6bebff690215045d73
RaminMajidi <raminmajididev01@gmail.com>
2022-07-11 01:13:49 +0430

    Challenge Part 4

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const menuBtn = document.getElementById("menuBtn"),
 heartIcon = document.querySelector(".bx-heart"),
-menuContainer = document.querySelector(".menu-container");
+menuContainer = document.querySelector(".menu-container"),
+moodBtn = document.getElementById("mood"),
+Body = document.getElementById("Body");
 
 menuBtn.addEventListener("click",()=>{
     menuBtn.classList.toggle("bx-menu");
@@ -10,5 +12,13 @@ menuBtn.addEventListener("click",()=>{
 
 heartIcon.addEventListener("click",()=>{
     heartIcon.classList.toggle("like");
-})
+});
+
+moodBtn.addEventListener("click",()=>{
+   Body.classList.contains("dark") ? moodBtn.setAttribute("title","Drak Mood") :  moodBtn.setAttribute("title","Light Mood") ;
+   Body.classList.toggle("dark");
+   moodBtn.classList.toggle("bxs-moon");
+   moodBtn.classList.toggle("bxs-sun");
+  
+});
 
